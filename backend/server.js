e536400b2c0addbc2b7649b84b3d3fbc23412ac1
Milan721166 <milansahoo7211662005@gmail.com>
2025-11-07 21:42:25 +0530

    add backend demo

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to ConnectHub Backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
